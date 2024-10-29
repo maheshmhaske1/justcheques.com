9e854bc02daa92565e0943294b084741ff6c138f
pravinpimpale <pravinpimpale45@gmail.com>
2024-10-29 18:38:16 +0530

    dashboard added

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -60,9 +60,9 @@
                 <div class="content-wrapper">
                     <!-- Content -->
                     <div class="container-xxl flex-grow-1 container-p-y">
-                        <div class="row">
+                        {{-- <div class="row"> --}}
                             @yield('content')
-                        </div>
+                        {{-- </div> --}}
                     </div>
                     <!-- / Content -->
 
--- resources/views/admin/partials/dashboard/dashboard.blade.php
+++ resources/views/admin/partials/dashboard/dashboard.blade.php
@@ -1,4 +1,73 @@
 @extends('admin.admin')
 @section('content')
-<h1>Hello</h1>
+<div class="row g-6 mb-6">
+  <div class="col-sm-6 col-xl-3">
+    <div class="card">
+      <div class="card-body">
+        <div class="d-flex align-items-start justify-content-between">
+          <div class="content-left">
+            <span class="text-heading">Total Users</span>
+            <div class="d-flex align-items-center my-1">
+              <h4 class="mb-0 me-2">21,459</h4>
+            </div>
+          </div>
+          <div class="avatar">
+              <i class="mdi mdi-account-multiple-outline bx-lg text-success"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 col-xl-3">
+    <div class="card">
+      <div class="card-body">
+        <div class="d-flex align-items-start justify-content-between">
+          <div class="content-left">
+            <span class="text-heading">Total Orders</span>
+            <div class="d-flex align-items-center my-1">
+              <h4 class="mb-0 me-2">4,567</h4>
+            </div>
+          </div>
+          <div class="avatar">
+              <i class="mdi mdi-border-all bx-lg text-primary"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 col-xl-3">
+    <div class="card">
+      <div class="card-body">
+        <div class="d-flex align-items-start justify-content-between">
+          <div class="content-left">
+            <span class="text-heading">Total Customer</span>
+            <div class="d-flex align-items-center my-1">
+              <h4 class="mb-0 me-2">19,860</h4>
+            </div>
+          </div>
+          <div class="avatar">
+              <i class="mdi mdi-account-group-outline bx-lg text-primary"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 col-xl-3">
+    <div class="card">
+      <div class="card-body">
+        <div class="d-flex align-items-start justify-content-between">
+          <div class="content-left">
+            <span class="text-heading">Total Vendors</span>
+            <div class="d-flex align-items-center my-1">
+              <h4 class="mb-0 me-2">237</h4>
+            </div>
+          </div>
+          <div class="avatar">
+              <i class="mdi mdi-account-supervisor-outline bx-lg text-warning"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 @endsection
